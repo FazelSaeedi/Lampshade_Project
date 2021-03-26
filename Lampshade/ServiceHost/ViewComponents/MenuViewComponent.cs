76d784c12afc336a5f3a93eaf4742b6aae6ce968
Fazel Saeedi <fazels36@gmail.com>
2021-03-26 16:33:07 +0430

    Footer and Menu Move to Seperated View Component

---
+++ Lampshade/ServiceHost/ViewComponents/MenuViewComponent.cs
@@ -0,0 +1,32 @@
+﻿using _01_LampshadeQuery;
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.CompilerServices;
+using _01_LampShadeQuery.Contract.ProductCategory;
+
+namespace ServiceHost.ViewComponents
+{
+    public class MenuViewComponent : ViewComponent
+    {
+        private readonly IProductCategoryQuery _productCategoryQuery;
+        //private readonly IArticleCategoryQuery _articleCategoryQuery;
+        public MenuViewComponent(IProductCategoryQuery productCategoryQuery)
+        {
+            //_articleCategoryQuery = articleCategoryQuery;
+            _productCategoryQuery = productCategoryQuery;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            /*var result = new MenuModel
+            {
+                //ArticleCategories = _articleCategoryQuery.GetArticleCategories(),
+                ProductCategories = _productCategoryQuery.GetProductCategories()
+            };
+            
+             result
+
+             */
+            return View();
+        }
+    }
+}
